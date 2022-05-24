e5a0f21696d9cc0ea87394501a1e469926d169a5
Phani Raj <phani@planetscale.com>
2022-05-24 12:04:07 -0500

    debug docker push failing

--- script/release.sh
+++ script/release.sh
@@ -33,7 +33,7 @@ cd /tmp && tar -zxvf goreleaser_Linux_x86_64.tar.gz
 
 echo "--- running goreleaser"
 
-echo "Login to the docker..."
+echo "Login to the docker with user ${DOCKER_USERNAME}..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 export GORELEASER_CURRENT_TAG=$(buildkite-agent meta-data get "release-version")
