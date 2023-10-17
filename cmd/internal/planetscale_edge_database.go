307616d6cdbe685538e23d2a33db1e4f26164546
Liz van Dijk <lizztheblizz@gmail.com>
2023-10-17 13:37:05 +0100

    removed random import

--- cmd/internal/planetscale_edge_database.go
+++ cmd/internal/planetscale_edge_database.go
@@ -7,8 +7,6 @@ import (
 	"net/http"
 	"strings"
 	"time"
-
-	"github.com/go-sql-driver/mysql"
 	"github.com/pkg/errors"
 	psdbconnect "github.com/planetscale/airbyte-source/proto/psdbconnect/v1alpha1"
 	"github.com/planetscale/psdb/auth"
