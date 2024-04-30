730b2a01eb4698292856e9441f8fccc7e35318aa
notfelineit <notfelineit@gmail.com>
2024-04-30 11:05:23 -0700

    Add test

--- cmd/internal/planetscale_connection_test.go
+++ cmd/internal/planetscale_connection_test.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"fmt"
 	"testing"
 
 	psdbconnect "github.com/planetscale/airbyte-source/proto/psdbconnect/v1alpha1"
@@ -123,6 +124,40 @@ func TestCanGenerateInitialState_Unsharded(t *testing.T) {
 			Shard:    shard,
 			Keyspace: "connect-test",
 			Position: "",
+		})
+		assert.NoError(t, err)
+		expectedShardStates.Shards[shard] = cursor
+	}
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedShardStates, shardStates)
+}
+
+func TestCanGenerateInitialState_StartingGtids(t *testing.T) {
+	psc := PlanetScaleSource{
+		Host:          "useast.psdb.connect",
+		Username:      "usernameus-east-4",
+		Password:      "pscale_password",
+		Database:      "connect-test",
+		StartingGtids: "{\"connect-test\":{\"-40\":\"MySQL56/MYGTID1:1-3\",\"40-80\":\"MySQL56/MYGTID2:1-3\",\"80-c0\":\"MySQL56/MYGTID3:1-3\",\"c0-\":\"MySQL56/MYGTID4:1-3\"}}",
+	}
+	shards := []string{
+		"-40",
+		"40-80",
+		"80-c0",
+		"c0-",
+	}
+	shardStates, err := psc.GetInitialState("connect-test", shards)
+	assert.NoError(t, err)
+	expectedShardStates := ShardStates{
+		Shards: map[string]*SerializedCursor{},
+	}
+
+	for i, shard := range shards {
+		cursor, err := TableCursorToSerializedCursor(&psdbconnect.TableCursor{
+			Shard:    shard,
+			Keyspace: "connect-test",
+			Position: fmt.Sprintf("MySQL56/MYGTID%s:1-3", fmt.Sprint(i+1)),
 		})
 		assert.NoError(t, err)
 		expectedShardStates.Shards[shard] = cursor
